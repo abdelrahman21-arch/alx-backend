b6eb66bb97bdc573ed18408850aa1ac661eb98ae
abdelrahman21-arch <boodyhussein12@gmail.com>
2024-06-27 02:10:27 +0300

    - LIFO

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -20,16 +20,18 @@ class LIFOCache(BaseCaching):
         :param item: value
         :return: new dict according to fifo
         """
-        if key is None or item is None:
-            return
+        if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                if key in self.cache_data:
+                    del self.cache_data[key]
+                    self.key_indexes.remove(key)
+                else:
+                    del self.cache_data[self.key_indexes[self.MAX_ITEMS - 1]]
+                    item_discarded = self.key_indexes.pop(self.MAX_ITEMS - 1)
+                    print("DISCARD:", item_discarded)
 
-        if len(self.cache_data) >= self.MAX_ITEMS:
-            first_key = self.key_indexes.pop(-1) # get first key
-            print(f"DISCARD:{first_key}\n")  # pop first item
-            del self.cache_data[first_key]  # remove first item
-
-        self.cache_data[key] = item
-        self.key_indexes.append(key)
+            self.cache_data[key] = item
+            self.key_indexes.append(key)
 
     def get(self, key):
         """
@@ -37,7 +39,6 @@ class LIFOCache(BaseCaching):
         :param key: key
         :return: item
         """
-        if key is None:
-            return None
-
-        return self.cache_data.get(key, None)
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
