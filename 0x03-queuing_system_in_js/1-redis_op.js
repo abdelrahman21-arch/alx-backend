36c7c49651619bc0bb77d479619870a99057d4e2
abdelrahman21 <boodyhussein12@gmail.com>
2024-07-31 19:00:25 +0300

    set - get redis done

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,39 @@
+import { createClient } from 'redis';
+const redis = require('redis');
+
+
+const client = createClient({
+  url: 'redis://127.0.0.1:6379' // Adjust URL if necessary
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error('Redis client not connected to the server:', err.message);
+});
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName){
+
+    client.get(schoolName, (err, reply) => {
+	if (err) {
+	    console.error(`Error fetching value:`, err);
+	}
+	else {
+	    console.log(`${reply}`);
+	    
+	}
+
+    });
+}
+
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
+
